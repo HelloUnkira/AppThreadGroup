87e24e4a3021918ca8dd40e2af4cedae55808f86
HelloUnkira <99794704+HelloUnkira@users.noreply.github.com>
2024-09-20 16:18:39 +0800

    ...

--- AppThreadGroup/project_watch/app_thread/app_thread_scui/scui/draw/scui_draw_basic.c
+++ AppThreadGroup/project_watch/app_thread/app_thread_scui/scui/draw/scui_draw_basic.c
@@ -44,17 +44,28 @@ void scui_draw_area_blur(scui_surface_t *dst_surface, scui_area_t *dst_clip)
      int32_t gauss_ofs = GAUSS_SCALA / 2;
     uint32_t gauss_sum = 0;
     uint32_t gauss_cof[GAUSS_SCALA][GAUSS_SCALA] = {
+        #if 1
+        /* 如果是高斯模糊,这里填入量化后的高斯模糊矩阵 */
+        0,
+        #else
         {1,  4,  7,  4, 1},
         {4, 16, 26, 16, 4},
         {7, 26, 41, 26, 7},
         {4, 16, 26, 16, 4},
         {1,  4,  7,  4, 1},
+        #endif
     };
+    
     uintptr_t pixel_ofs[GAUSS_SCALA][GAUSS_SCALA] = {0};
     scui_color_wt_t pixel_mat[GAUSS_SCALA][GAUSS_SCALA] = {0};
     
     for (int8_t idx_j = 0; idx_j < GAUSS_SCALA; idx_j++)
     for (int8_t idx_i = 0; idx_i < GAUSS_SCALA; idx_i++) {
+         #if 1
+         // 这里是平滑模糊矩阵,所有矩阵值均为1
+         // 如果是高斯模糊矩阵,注销此处
+         gauss_cof[idx_j][idx_i] = 1;
+         #endif
          pixel_ofs[idx_j][idx_i] = 0;
          pixel_ofs[idx_j][idx_i] += - dst_line * (idx_j + gauss_ofs - (GAUSS_SCALA - 1));
          pixel_ofs[idx_j][idx_i] += - dst_byte * (idx_i + gauss_ofs - (GAUSS_SCALA - 1));
